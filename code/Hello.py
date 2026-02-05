113a0cf562e4618c7aab1cfe54f4df4fb7cd7609
craventrob-hash <craventrob@gmail.com>
2026-02-05 10:38:00 -0500

    session 5
    test

--- code/Hello.py
+++ code/Hello.py
@@ -1,1 +1,8 @@
-print('Hello, World!')
+import time
+
+def groundhog_day():
+    print("Did you mean groundhog day?")
+    time.sleep(1)
+    groundhog_day()
+
+groundhog_day( )
---
+++ code/session5.py
@@ -0,0 +1,6 @@
+#a product would cost 100$ how much tax do we pay
+
+product = 100 # in dollars
+tax_rate = 0.0625
+tax = product * tax_rate
+print(f'the tax for a product which costs${product} is ${tax}')
